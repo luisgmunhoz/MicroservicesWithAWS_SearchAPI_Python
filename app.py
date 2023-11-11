cc6f4af169ad07ad54583f922d2b60d76b6e7e1c
Luis Guilherme de Souza Munhoz <luisguilherme.sm@hotmail.com>
2023-11-11 12:30:32 -0300

    Final improvement for this PR

--- app.py
+++ app.py
@@ -86,9 +86,5 @@ def health_check() -> JSONResponse:
     return JSONResponse(status_code=200, content="OK")
 
 
-def main() -> None:
-    uvicorn.run(app, host="0.0.0.0", port=80)
-
-
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=80)
